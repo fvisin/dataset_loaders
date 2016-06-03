6b3e8678e51800e02fb975f642e251ea5f7979e2
Francesco Visin <fvisin@gmail.com>
2016-06-02 21:33:45 -0400

    Add dataset objects to first level imports

---
+++ dataset_loaders/__init__.py
@@ -0,0 +1,5 @@
+from images.camvid import CamvidDataset
+# from images.pascalvoc import PascalvocDataset
+from videos.gatech import GatechDataset
+from videos.changeD import ChangeDetectionDataset
+# from videos.colonoscopyVideos
