b5570a886f4db49431502d5a5fbc2e160614f7e3
Francesco Visin <fvisin@gmail.com>
2016-06-05 15:42:36 -0400

    Fix PascalVOC

--- dataset_loaders/images/pascalvoc.py
+++ dataset_loaders/images/pascalvoc.py
@@ -1,65 +1,49 @@
-import time
 import os
 import numpy as np
 from skimage import io
 from PIL import Image
 
-from batchiterator import BatchIterator, threaded_generator
-from dataset_helpers import convert_01c_to_c01
 import dataset_loaders
+from dataset_loaders.parallel_loader import ThreadedDataset
+
+class_ids = {'255': 21}
 
 
-class VOCdataset(BatchIterator):
+class VOCdataset(ThreadedDataset):
     name = 'pascal_voc'
-    nclasses = 21
-    debug_shape = (100, 100, 3)
+    nclasses = 22
+    debug_shape = (375, 500, 3)
 
     data_shape = (None, None, 3)
-    mean = 0.
+    mean = np.asarray([122.67891434, 116.66876762, 104.00698793]).astype(
+        'float32')
     std = 1.
+    void_labels = [21]
 
     def __init__(self,
                  which_set="train",
                  with_filenames=False,
-                 threshold_masks=False,
-                 data_format="b01c",
-                 year="VOC2012",
-                 predictions=False,
-                 normalize=True,
+                 with_predictions=False,
                  with_teacher=False,
                  teacher_temperature=1,
-                 minibatch_size=1, *args, **kwargs):
+                 year="VOC2012",
+                 *args, **kwargs):
 
-        self.which_set = which_set
+        self.which_set = "val" if which_set == "valid" else which_set
+        if self.which_set == "test" and year != "VOC2012":
+            raise ValueError("No test set for other than 2012 year")
+        if self.which_set == 'test':
+            self.has_GT = False
         self.with_filenames = with_filenames
+        self.with_predictions = with_predictions
+        self.with_teacher = with_teacher
+        self.teacher_temperature = teacher_temperature
+        self.year = year
+
         self.path = os.path.join(
             dataset_loaders.__path__[0], 'datasets', 'PASCAL-VOC',
             'VOCdevkit')
         self.sharedpath = '/data/lisa/data/PASCAL-VOC/VOCdevkit'
-        self.year = year
-
-        if self.which_set == "test" and year == "2012":
-            raise ValueError("No test set for other than 2012 year")
-
-        self.threshold_masks = threshold_masks
-        self.data_format = data_format
-        self.random_state = np.random.RandomState(1999)
-        self.predictions = predictions
-        self.normalize = normalize
-        self.with_teacher = with_teacher
-        self.teacher_temperature = teacher_temperature
-        self.n_classes = 21
-        self.image_shape = [3, None, None]
-
-        self.image_shape_out = [self.n_classes] + self.image_shape[1:]
-
-        if self.which_set == "test":
-            testing = True
-        else:
-            testing = False
-
-        # TEMPORARY
-        self.path = self.sharedpath
 
         self.txt_path = os.path.join(self.path, self.year,
                                      "ImageSets", "Segmentation")
@@ -69,9 +53,7 @@ class VOCdataset(BatchIterator):
         self.pred_path = os.path.join(self.path, self.which_set,
                                       "teacher_pred_temp1")
 
-        data_list = self.get_names()
-
-        super(VOCdataset, self).__init__(minibatch_size, data_list, testing)
+        super(VOCdataset, self).__init__(*args, **kwargs)
 
     def get_names(self):
         # Get filenames for the current set
@@ -100,61 +82,39 @@ class VOCdataset(BatchIterator):
         # Return images
         return image_names
 
-    def fetch_from_dataset(self, batch_to_load):
+    def load_sequence(self, img_name):
         image_batch = []
         mask_batch = []
         pred_batch = []
         filename_batch = []
 
-        for img_name in batch_to_load:
-            # Load image
-            try:
-                img = io.imread(os.path.join(self.image_path, img_name +
-                                             ".jpg"))
-            except IOError:
-                print("Failed to load " + img_name + ".jpg")
-                continue
+        if self.seq_length != 1:
+            raise NotImplementedError()
 
-            # Load mask
-            try:
-                if self.which_set != "test":
-                    mask = np.asarray(Image.open(os.path.join(self.mask_path,
-                                                              img_name +
-                                                              ".png")))
-            except IOError:
-                print("Failed to load " + img_name + ".png")
-                continue
-            # Load teacher predictions and soft predictions
-            try:
-                if self.predictions:
-                    # Predictions
-                    pred = np.load(os.path.join(self.pred_path, img_name +
-                                                ".npy"))
+        # Load image
+        img = io.imread(os.path.join(self.image_path, img_name +
+                                     ".jpg"))
+        img = img / 255.
 
-            except IOError:
-                print("Failed to load teacher prediction" +
-                      img_name + ".npy")
-                continue
+        # Load mask
+        if self.which_set != "test":
+            mask = np.array(Image.open(
+                os.path.join(self.mask_path, img_name + ".png")))
+            mask[mask == 255] = 21
 
-            # Convert image to c01 format
-            if self.data_format == "bc01":
-                img = convert_01c_to_c01(img)
+        # Load teacher predictions and soft predictions
+        if self.with_predictions:
+            pred = np.load(os.path.join(self.pred_path, img_name +
+                                        ".npy"))
 
-            # Normalize
-            if self.normalize:
-                mean_subs = np.asarray([122.67891434,
-                                        116.66876762,
-                                        104.00698793])
-                img = np.float32(img - mean_subs[None, None, :])
-
-            # Add to minibatch
-            image_batch.append(img)
-            if self.which_set != "test":
-                mask_batch.append(mask)
-            if self.predictions:
-                pred_batch.append(pred)
-            if self.with_filenames:
-                filename_batch.append(img_name)
+        # Add to minibatch
+        image_batch.append(img)
+        if self.which_set != "test":
+            mask_batch.append(mask)
+        if self.with_predictions:
+            pred_batch.append(pred)
+        if self.with_filenames:
+            filename_batch.append(img_name)
 
         ret = [np.array(image_batch), np.array(mask_batch)]
 
@@ -165,38 +125,16 @@ class VOCdataset(BatchIterator):
             other += [pred_batch]
 
         # return image_batch, mask_batch, batch_to_load, pred_batch
-        return ret + other
-
-    def get_image_shape(self):
-        return self.image_shape
-
-    def get_image_shape_out(self):
-        return self.image_shape_out
-
-    def get_n_classes(self):
-        return self.n_classes
-
+        return tuple(ret + other)
 
 if __name__ == '__main__':
-    d = VOCdataset(which_set="train",
-                   data_format="bc01",
-                   minibatch_size=500,
-                   predictions=False,
-                   normalize=True,
-                   teacher_temperature=10)
+    dd = VOCdataset(which_set='test',
+                    shuffle_at_each_epoch=True,
+                    get_one_hot=True,
+                    get_01c=False,)
 
-    start = time.time()
-    max_epochs = 2
-    n_batches = d.get_n_batches()
-
-    d = threaded_generator(d, 10)
-
-    for epoch in range(max_epochs):
-        for mb in range(n_batches):
-            image_batch, mask_batch, filenames, pred_batch = d.next()
-            # time.sleep approximates running some model
-            time.sleep(1)
-            stop = time.time()
-            tot = stop - start
-            print("Threaded time: %s" % (tot))
-            print("Minibatch %s" % str(mb))
+    print('Tot {}'.format(dd.epoch_length))
+    for i, _ in enumerate(range(dd.epoch_length)):
+        dd.next()
+        if i % 20 == 0:
+            print str(i)
