9525e94a7648b0b1da6afc3acab8be7d926a72ce
Francesco Visin <fvisin@gmail.com>
2016-06-05 15:42:07 -0400

    Add pascalvoc to init

--- dataset_loaders/__init__.py
+++ dataset_loaders/__init__.py
@@ -1,5 +1,5 @@
 from images.camvid import CamvidDataset
-# from images.pascalvoc import PascalvocDataset
+from images.pascalvoc import VOCdataset
 from videos.gatech import GatechDataset
 from videos.changeD import ChangeDetectionDataset
 # from videos.colonoscopyVideos
