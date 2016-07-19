1c9923dc4cb748324786ace22349df6faa00e58e
Francesco Visin <fvisin@gmail.com>
2016-07-19 15:06:41 -0400

    Improve warning message

--- dataset_loaders/parallel_loader.py
+++ dataset_loaders/parallel_loader.py
@@ -294,13 +294,15 @@ class ThreadedDataset(object):
                 if crop[0] < height:
                     top = self.rng.randint(height - crop[0])
                 else:
-                    print('Dataset loader: Crop size exceeds image size')
+                    print('Dataset loader: Crop height greater or equal to '
+                          'image size')
                     top = 0
                     crop[0] = height
                 if crop[1] < width:
                     left = self.rng.randint(width - crop[1])
                 else:
-                    print('Dataset loader: Crop size exceeds image size')
+                    print('Dataset loader: Crop width greater or equal to '
+                          'image size')
                     left = 0
                     crop[1] = width
 
