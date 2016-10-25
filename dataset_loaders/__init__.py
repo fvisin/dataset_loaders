cb7ab4dae5cc6394f44a4e91a15ce97bd9d94d46
Francesco Visin <fvisin@gmail.com>
2016-10-25 12:59:09 -0400

    PEP8

--- dataset_loaders/__init__.py
+++ dataset_loaders/__init__.py
@@ -4,7 +4,7 @@ from images.em_stacks import IsbiEmStacksDataset
 from images.pascalvoc import VOCdataset
 from images.polyps300 import Polyps300Dataset
 
-from videos.davis import DavisDataset
 from videos.change_detection import ChangeDetectionDataset
 from videos.colonoscopyVideos import PolypVideoDataset
+from videos.davis import DavisDataset
 from videos.gatech import GatechDataset
