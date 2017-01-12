f8017f7edc92a4c73743c66a786adafc3bdc0423
Francesco Visin <fvisin@gmail.com>
2017-01-12 15:21:35 +0100

    Add pytest to check the data of every dataset

--- dataset_loaders/test/test_all.py
+++ dataset_loaders/test/test_all.py
@@ -1,34 +1,39 @@
 import time
-import unittest
+import pytest
 
 from dataset_loaders import *
+datasets = [el for el in dir() if 'Dataset' in el]
 
 
-class TestDatasets(unittest.TestCase):
+@pytest.mark.parametrize('dataset', [
+        eval(dataset) for dataset in datasets])
+def testOneDataset(dataset, verbose=False):
+    '''Test the data of all the datasets
 
-    def testAll(self, verbose=False):
-        datasets = [el for el in dir() if 'Dataset' in el]
-        for dataset in datasets:
-            for which_set in ['train', 'val', 'test']:
-                print('Testing ' + dataset.name + ' - ' + which_set)
-                d = dataset(
-                    which_set=which_set,
-                    batch_size=1,
-                    seq_length=0,  # 4D
-                    raise_IOErrors=True)
-                start = time.time()
-                tot = 0
+    This tests all the datasets that are imported through the dataset
+    loaders __init__. The test only checks that all the files are
+    readable. Run as `pytest -s test_all`'''
+    for which_set in ['train', 'val', 'test']:
+        print('\n******************************************')
+        print('Testing ' + dataset.name + ' - ' + which_set)
+        d = dataset(
+            which_set=which_set,
+            batch_size=1,
+            seq_length=0,  # 4D
+            raise_IOErrors=True)
+        start = time.time()
+        tot = 0
 
-                for mb in range(d.epoch_length):
-                    batch = d.next()
-                    self.assertIsNotNone(batch, 'The batch was empty.')
+        for mb in range(d.nbatches):
+            batch = d.next()
+            assert batch is not None, 'The batch was empty.'
 
-                    # time.sleep approximates running some model
-                    time.sleep(0.1)
-                    if verbose:
-                        stop = time.time()
-                        part = stop - start - 1
-                        start = stop
-                        tot += part
-                        print('Minibatch %s time: %s (%s)' % (
-                            str(mb), part, tot))
+            # time.sleep approximates running some model
+            time.sleep(0.1)
+            if verbose:
+                stop = time.time()
+                part = stop - start - 1
+                start = stop
+                tot += part
+                print('Minibatch %s time: %s (%s)' % (
+                    str(mb), part, tot))
