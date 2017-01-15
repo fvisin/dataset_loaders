1db199a76563b329ad583e475990e7539a13963c
Francesco Visin <fvisin@gmail.com>
2017-01-15 17:57:00 +0100

    Fix flip_axis

--- dataset_loaders/data_augmentation.py
+++ dataset_loaders/data_augmentation.py
@@ -128,13 +128,14 @@ def random_channel_shift(x, intensity, rows_idx, cols_idx, chan_idx):
     return x
 
 
-def flip_axis(x_in, axis):
-    x_out = np.zeros(x_in.shape, dtype=x_in.dtype)
-    for i, x in enumerate(x_in):
-        x = np.asarray(x).swapaxes(axis, 0)
-        x = x[::-1, ...]
-        x_out[i] = x.swapaxes(0, axis)
-    return x_out
+def flip_axis(x, flipping_axis):
+    pattern = [flipping_axis]
+    pattern += [el for el in range(x.ndim) if el != flipping_axis]
+    inv_pattern = [pattern.index(el) for el in range(x.ndim)]
+    x = x.transpose(pattern)  # "flipping_axis" first
+    x = x[::-1, ...]
+    x = x.transpose(inv_pattern)
+    return x
 
 
 # Define warp
