3b3cf4195ed5b6c06fd980181e080babe7f73f92
Francesco Visin <fvisin@gmail.com>
2017-01-31 12:55:47 +0100

    Remove old datasets

--- dataset_loaders/__init__.py
+++ dataset_loaders/__init__.py
@@ -2,8 +2,6 @@ from images.camvid import CamvidDataset
 from images.cityscapes import CityscapesDataset
 from images.isbi_em_stacks import IsbiEmStacksDataset
 from images.kitti import KITTIdataset
-from images.kitti2 import KITTI2dataset
-from images.kitti3 import KITTI3dataset
 from images.mscoco import MSCocoDataset
 from images.pascalvoc import PascalVOCdataset
 from images.polyps912 import Polyps912Dataset
@@ -12,4 +10,3 @@ from images.scene_parsing_MIT import SceneParsingMITDataset
 from videos.change_detection import ChangeDetectionDataset
 from videos.davis import DavisDataset
 from videos.gatech import GatechDataset
-from videos.polyps_videos import PolypVideoDataset
